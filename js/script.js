c10f6deb3efd7db2d29d24f37bdb4aa7d87f3ea2
Brendan Crawford <brendan@wholepunk.com>
2018-08-29 14:10:44 -0700

    updated countdown with the correct date

--- js/script.js
+++ js/script.js
@@ -483,7 +483,7 @@
 	// 06.2 Countdown
 	//================================================================================
 	var theday = new Date();
-	theday = new Date(2018, 9, 2);
+	theday = new Date(2018, 8, 2);
 	$('.countdown').countdown({until: theday, format: 'DHMS'});
 	
 	
